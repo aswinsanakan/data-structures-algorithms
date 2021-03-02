358ea4bc2c453acb45d48285f9e2d91aad72c0e5
Aswin Sanakan <aswinsanakan@gmail.com>
2021-03-02 13:39:57 +0530

    Create O(1).js

---
+++ BigO-examples/O(1).js
@@ -0,0 +1,10 @@
+// O(1) - Constant time
+
+const boxes = new Array().fill("box");
+const boxes = ["box1", "box2", "box3"];
+
+function getFirstBox(boxes) {
+  console.log(boxes(0)); // Always takes only the first box
+}
+
+getFirstBox(boxes);
