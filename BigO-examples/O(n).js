d44ddbb62ccd3a4ba5743f4b0cbc1ee123816b49
Aswin Sanakan <aswinsanakan@gmail.com>
2021-03-02 13:36:25 +0530

    Create O(n).js

---
+++ BigO-examples/O(n).js
@@ -0,0 +1,13 @@
+// O(n)
+
+const nemo = new Array(100).fill("nemo");
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+}
+
+findNemo(nemo);
